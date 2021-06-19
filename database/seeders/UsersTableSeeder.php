c40f84a487037b45f4ab3808b244f549b2cd3fa9
1113373 <s1113373@student.hsleiden.nl>
2021-06-19 14:13:58 +0200

    Fixed duplicate entry in usertableseeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -59,7 +59,7 @@ class UsersTableSeeder extends Seeder
             'address' => 'Neptunus 49',
             'email' => 'matt@mail.com',
             'password' => bcrypt('bimpsert'),
-            'phone_number' => '0612345678',
+            'phone_number' => '0687654321',
             'date_of_birth' => Carbon::create(2001, 12, 11, 0),
             'external_cv' => 'https://www.linkedin.com/in/matt-verdoes-230728185/',
             'created_at' => now(),
