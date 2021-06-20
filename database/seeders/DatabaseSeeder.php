7fa497090b4a1c2f62496d1f5520650b44c82164
Tim <timesveldt@hotmail.com>
2021-06-20 17:01:16 +0200

    Fixed DatabaseSeeder class bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            JobsTableSeeder::class,
             UsersTableSeeder::class,
             RolesTableSeeder::class,
             PremiaTableSeeder::class,
