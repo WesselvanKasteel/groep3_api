deca585230730e96402b10b6a0809666d28b7e94
Wessel van Kasteel <s1115662@student.hsleiden.nl>
2021-06-25 19:17:55 +0200

    Removed cors middleware

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,15 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
