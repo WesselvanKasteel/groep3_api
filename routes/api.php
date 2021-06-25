258676069abbcacaa665e4c35109f23a9332a469
Wessel van Kasteel <s1115662@student.hsleiden.nl>
2021-06-25 18:42:33 +0200

    Added cors middleware

--- routes/api.php
+++ routes/api.php
@@ -27,54 +27,57 @@ use App\Models\Education;
 |
 */
 
-
-Route::group(['prefix' => 'auth'], function() {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::post('/me', [AuthController::class, 'me']);
-    Route::get('/check-user-role', [AuthController::class, 'checkUserRole']);
+Route::group(['middleware' => 'cors'], function() {
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::post('/me', [AuthController::class, 'me']);
+        Route::get('/check-user-role', [AuthController::class, 'checkUserRole']);
+    });
+    
+    
+    Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
+        Route::get('/', [ProfileController::class, 'show']);
+        Route::put('/edit', [ProfileController::class, 'edit']);
+        Route::post('/edit/picture', [ProfileController::class, 'uploadProfilePicture']);
+        Route::post('/edit/video', [VideoController::class, 'updateProfile']);
+    });
+    
+    Route::group(['prefix' => 'profile', 'middleware' => 'auth:api'], function() {
+        Route::get('/users', [ProfileController::class, 'getUsersData']);
+    });
+    
+    Route::group(['prefix' => 'vacancy'], function() {
+        Route::get('/vacancies', [VacancyController::class, 'index']);
+    });
+    
+    Route::group(['prefix' => 'skill'], function() {
+        Route::get('/skills', [SkillController::class, 'getAllSkills']);
+    });
+    
+    Route::group(['prefix' => 'vacancy', 'middleware' => 'auth:api'], function() {
+        Route::get('/vacancy', [VacancyController::class, 'getVacancyData']);
+        Route::post('/store', [VideoController::class, 'store']);
+    });
+    
+    Route::group(['prefix' => 'skills', 'middleware' => 'auth:api'], function() {
+        Route::get('/', [SkillController::class, 'index']);
+        Route::post('/store', [SkillController::class, 'store']);
+    });
+    
+    Route::group(['prefix' => 'jobs', 'middleware' => 'auth:api'], function() {
+        Route::get('/', [JobController::class, 'index']);
+        Route::post('/store', [JobController::class, 'store']);
+        Route::delete('/destroy', [JobController::class, 'destroy']);
+    });
+    
+    Route::group(['prefix' => 'education', 'middleware' => 'auth:api'], function() {
+        Route::get('/', [EducationController::class, 'index']);
+        Route::post('/store', [EducationController::class, 'store']);
+    });    
 });
 
 
-Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function() {
-    Route::get('/', [ProfileController::class, 'show']);
-    Route::put('/edit', [ProfileController::class, 'edit']);
-    Route::post('/edit/picture', [ProfileController::class, 'uploadProfilePicture']);
-    Route::post('/edit/video', [VideoController::class, 'updateProfile']);
-});
-
-Route::group(['prefix' => 'profile', 'middleware' => 'auth:api'], function() {
-    Route::get('/users', [ProfileController::class, 'getUsersData']);
-});
-
-Route::group(['prefix' => 'vacancy'], function() {
-    Route::get('/vacancies', [VacancyController::class, 'index']);
-});
-
-Route::group(['prefix' => 'skill'], function() {
-    Route::get('/skills', [SkillController::class, 'getAllSkills']);
-});
-
-Route::group(['prefix' => 'vacancy', 'middleware' => 'auth:api'], function() {
-    Route::get('/vacancy', [VacancyController::class, 'getVacancyData']);
-    Route::post('/store', [VideoController::class, 'store']);
-});
-
-Route::group(['prefix' => 'skills', 'middleware' => 'auth:api'], function() {
-    Route::get('/', [SkillController::class, 'index']);
-    Route::post('/store', [SkillController::class, 'store']);
-});
-
-Route::group(['prefix' => 'jobs', 'middleware' => 'auth:api'], function() {
-    Route::get('/', [JobController::class, 'index']);
-    Route::post('/store', [JobController::class, 'store']);
-    Route::delete('/destroy', [JobController::class, 'destroy']);
-});
-
-Route::group(['prefix' => 'education', 'middleware' => 'auth:api'], function() {
-    Route::get('/', [EducationController::class, 'index']);
-    Route::post('/store', [EducationController::class, 'store']);
-});    
 
